d5c34d040375a81865aead119df81b37de489a93
Krish <567krish.kumar@gmail.com>
2026-05-11 01:05:25 +0530

    Fix:RSTP server issue

--- raspberry_pi/camera_stream/RTSP_server.py
+++ raspberry_pi/camera_stream/RTSP_server.py
@@ -24,15 +24,20 @@ class SkyRangerFactory(GstRtspServer.RTSPMediaFactory):
 
         super(SkyRangerFactory, self).__init__()
 
+        # Allow multiple clients
         self.set_shared(True)
+
+        # Reduce latency
+        self.set_latency(0)
 
     def do_create_element(self, url):
 
         pipeline = """
             v4l2src device=/dev/video8 do-timestamp=true !
-            video/x-raw,width=640,height=480,framerate=30/1 !
+            video/x-raw,width=640,height=480 !
+            videorate !
+            video/x-raw,framerate=25/1 !
             videoconvert !
-            queue !
             x264enc tune=zerolatency speed-preset=ultrafast bitrate=1000 !
             h264parse !
             rtph264pay config-interval=1 name=pay0 pt=96
@@ -51,14 +56,18 @@ class SkyRangerRTSPServer:
 
         self.server = GstRtspServer.RTSPServer()
 
+        # RTSP Port
         self.server.set_service("8554")
 
+        # Create factory
         factory = SkyRangerFactory()
 
+        # Mount stream
         mounts = self.server.get_mount_points()
 
         mounts.add_factory("/fpv", factory)
 
+        # Attach server
         self.server.attach(None)
 
         print("\n===================================")
